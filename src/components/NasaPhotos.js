e1e631fb5f308e6285147d5ef9d67349dca37054
prashishb <prashb.web@gmail.com>
2021-10-07 15:56:02 -0400

    added media query

--- src/components/NasaPhotos.js
+++ src/components/NasaPhotos.js
@@ -18,11 +18,11 @@ export default function NasaPhotos() {
     return (
         <StyledDiv>
             <img src={photo.url} alt={photo.title} />
-            <StyledDetails>
+            <div>
                 <h2>{photo.title}</h2>
                 <span>{photo.date}</span>
                 <p>{photo.explanation}</p>
-            </StyledDetails>
+            </div>
         </StyledDiv>
     )
 }
@@ -31,23 +31,37 @@ const StyledDiv = styled.div`
     display: flex;
     justify-content: space-between;
     max-height: 100%;
-    max-width: 80%;
+    width: 80%;
     margin: 0 auto;
     padding: 2%;
     border: 1px solid #d2d2d2;
     border-radius: 2%;
     box-shadow: 0px 1px 6px -2px #807f7f;
+    font-size: 20px;
 
     img {
         width: 50%;
         object-fit: contain;
     }
-`
 
-const StyledDetails = styled.div`
-    width: 45%;
+    div {
+        width: 45%;
+    }
 
     p {
-        line-height: 1.1.%;
+        line-height: 1.5;
+    }
+
+    @media (max-width: 1280px) {
+        display: block;
+        font-size: 16px;
+
+        img {
+            width: 100%;
+        }
+
+        div {
+            width: 100%;
+        }
     }
 `
