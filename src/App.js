c896361618731f190cfa01a0174a8a7d083d9fb6
prashishb <prashb.web@gmail.com>
2021-10-07 15:21:26 -0400

    navbar styling

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
 
   return (
-    <div>
+    <div className="App">
       <NavBar />
       <NasaPhotos />
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function NavBar() {
     return (
-        <div class='nav'>
-            <h1>NASA Photo Of The Day</h1>
+        <div>
+            <StyledNav>
+                <h1>NASA Photo Of The Day</h1>
+            </StyledNav>
+            <StyledSpacer></StyledSpacer>
         </div>
     )
 }
+
+const StyledNav = styled.div`
+    position: absolute;
+    top: 0;
+    width: 100%;
+    background-color: #343A40;
+`
+const StyledSpacer = styled.div`
+    margin-bottom: 20%;
+`
